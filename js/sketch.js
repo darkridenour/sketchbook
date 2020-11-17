825557c47d553b6863c2d683b14aca95c6bcc556
Darien Ridenour <darienridenour@Dariens-MacBook-Pro.local>
2020-11-17 10:38:39 -0600

    fixed all the images

--- js/sketch.js
+++ js/sketch.js
@@ -20,7 +20,45 @@ let rachel;
 let strawberry;
 let research;
 let identity;
-
+let happiness;
+let void;
+let meaningless;
+let janet;
+let bathtub;
+let foot;
+let goldface;
+let cover;
+let oops;
+let ugh;
+let it;
+let ouch;
+let eject;
+let complicated;
+let comic;
+let climate;
+let oneface;
+let twoface;
+let threeface;
+let toon;
+let self;
+let baggage;
+let bloom;
+let brogod;
+let complex;
+let god;
+let divine;
+let ashley;
+let bailey;
+let parker;
+let jakob;
+let kally;
+let shelbie;
+let clark;
+let kayla;
+let nigel;
+let taylor;
+let oliver;
+let loser;
 
 function preload{
 //high school art
@@ -49,23 +87,48 @@ function preload{
   strawberry = loadImage("saic freshman/21.jpg");
   research = loadImage("saic freshman/22.gif");
   identity = loadImage("saic freshman/23.gif");
-  <img src="saic freshman/happiness.jpg" alt="Happiness" width="200">
-  <img src="saic freshman/ah.jpg" alt="Void" width="200">
-  <img src="saic freshman/meaningless.jpg" alt="Meaningless" width="200">
-  <img src="saic freshman/janet.jpg" alt="Janet" width="200">
-  <img src="saic freshman/bath.jpg" alt="Bathtub" width="400">
-  <img src="saic freshman/foot pics.jpg" alt="Step In" width="400">
-  <img src="saic freshman/face.jpg" alt="Goldface" width="400">
-  //this mortal coil
-  <img src="saic freshman/cover pt 3.jpg" alt="TMC Cover" width="400">
-  <img src="saic freshman/oops.jpg" alt="Oops" width="400">
-  <img src="saic freshman/ugh.jpg" alt="Ugh" width="400">
-  <img src="saic freshman/ouch.jpg" alt="Ouch" width="400">
-  <img src="saic freshman/eject.jpg" alt="Eject" width="400">
-  <img src="saic freshman/can you do this.jpg" alt="Can You Do This?" width="400">
-  <img src="saic freshman/complicated.jpg" alt="It's Complicated" width="500">
-  <img src="saic freshman/tmccoverdoc.jpg" alt="TMC Cover" width="400">
-  <img src="saic freshman/tmc doc.jpg" alt="TMC Comic Book" width="400">
+  happiness = loadImage("saic freshman/24.jpg");
+  void = loadImage("saic freshman/25.jpg");
+  meaningless = loadImage("saic freshman/26.jpg");
+  janet = loadImage("saic freshman/27.jpg");
+  bathtub = loadImage("saic freshman/28.jpg");
+  foot = loadImage("saic freshman/29.jpg");
+  goldface = loadImage("saic freshman/30.jpg");
+  //---this mortal coil----
+  cover = loadImage("saic freshman/31.jpg");
+  oops = loadImage("saic freshman/32.jpg");
+  ugh = loadImage("saic freshman/33.jpg");
+  ouch = loadImage("saic freshman/34.jpg");
+  eject = loadImage("saic freshman/35.jpg");
+  it = loadImage("saic freshman/36.jpg");
+  complicated = loadImage("saic freshman/37.jpg");
+  comic = loadImage("saic freshman/38.jpg");
+  oliver = loadImage("saic freshman/39.jpg");
 //saic sophomore year
+  climate = loadImage("saic sophomore/40.jpg");
+  oneface = loadImage("saic sophomore/41.jpg");
+  twoface = loadImage("saic sophomore/42.jpg");
+  threeface = loadImage("saic sophomore/43.jpg");
+  toon = loadImage("saic sophomore/44.jpg");
+  self = loadImage("saic sophomore/45.jpg");
+  baggage = loadImage("saic sophomore/46.jpg");
+  loser = loadImage("saic sophomore/47.jpg")
+//saic junior year
+  bloom = loadImage("saic junior/48.jpg");
+  brogod = loadImage("saic junior/49.jpg");
+  divine = loadImage("saic junior/50.jpg");
+  complex = loadImage("saic junior/51.jpg");
+  god = loadImage("saic junior/52.jpg");
+//Commissions
+  ashley = loadImage("Commissions/53.jpg");
+  bailey = loadImage("Commissions/54.jpg");
+  kally = loadImage("Commissions/55.jpg");
+  parker = loadImage("commissions/56.jpg");
+  shelbie = loadImage("commissions/57.jpg");
+  clark = loadImage("Commissions/58.jpg");
+  kayla = loadImage("Commissions/59.jpg");
+  nigel = loadImage("Commissions/60.jpg");
+  jakob = loadImage("commissions/61.jpg");
+  taylor = loadImage("commissions/62.jpg");
 
 }
