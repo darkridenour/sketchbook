911dd57746190206d4223112dd688e4218571b40
Darien Ridenour <darienridenour@Dariens-MacBook-Pro.local>
2020-11-17 09:56:40 -0600

    trying my best

---
+++ js/sketch.js
@@ -0,0 +1,71 @@
+let bandaid;
+let emma;
+let domestic;
+let eyes;
+let faces;
+let floral;
+let hand;
+let landscape;
+let missing;
+let multiplepersonalities;
+let plotkin;
+let pulling;
+let purple;
+let redeye;
+let shelbie;
+let waV;
+let insicurities;
+let growth;
+let rachel;
+let strawberry;
+let research;
+let identity;
+
+
+function preload{
+//high school art
+  bandaid  = loadImage("high school art/0.jpg");
+	emma = loadImage("high school art/1.jpg");
+	domestic = loadImage("high school art/2.jpg");
+	eyes = loadImage("high school art/3.jpg");
+	faces = loadImage("high school art/4.jpg");
+  concentration = loadImage("high school art/5.jpg")
+	floral = loadImage("high school art/6.jpg");
+	hand = loadImage("high school art/7.jpg");
+  landscape = loadImage("high school art/8.jpg");
+	missing = loadImage("high school art/9.jpg");
+	multiplepersonalities = loadImage("high school art/10.jpg");
+	plotkin = loadImage("high school art/11.jpg");
+  primary = loadImage("high school art/12.jpg")
+	pulling = loadImage("high school art/13.jpg");
+	purple = loadImage("high school art/14.jpg");
+  redeye = loadImage("high school art/15.jpg");
+	shelbie = loadImage("high school art/16.jpg");
+	waV = loadImage("high school art/17.jpg");
+//saic freshman year
+  insicurities = loadImage("saic freshman/18.jpg");
+  growth = loadImage("saic freshman/19.jpg");
+  rachel = loadImage("saic freshman/20.jpg");
+  strawberry = loadImage("saic freshman/21.jpg");
+  research = loadImage("saic freshman/22.gif");
+  identity = loadImage("saic freshman/23.gif");
+  <img src="saic freshman/happiness.jpg" alt="Happiness" width="200">
+  <img src="saic freshman/ah.jpg" alt="Void" width="200">
+  <img src="saic freshman/meaningless.jpg" alt="Meaningless" width="200">
+  <img src="saic freshman/janet.jpg" alt="Janet" width="200">
+  <img src="saic freshman/bath.jpg" alt="Bathtub" width="400">
+  <img src="saic freshman/foot pics.jpg" alt="Step In" width="400">
+  <img src="saic freshman/face.jpg" alt="Goldface" width="400">
+  //this mortal coil
+  <img src="saic freshman/cover pt 3.jpg" alt="TMC Cover" width="400">
+  <img src="saic freshman/oops.jpg" alt="Oops" width="400">
+  <img src="saic freshman/ugh.jpg" alt="Ugh" width="400">
+  <img src="saic freshman/ouch.jpg" alt="Ouch" width="400">
+  <img src="saic freshman/eject.jpg" alt="Eject" width="400">
+  <img src="saic freshman/can you do this.jpg" alt="Can You Do This?" width="400">
+  <img src="saic freshman/complicated.jpg" alt="It's Complicated" width="500">
+  <img src="saic freshman/tmccoverdoc.jpg" alt="TMC Cover" width="400">
+  <img src="saic freshman/tmc doc.jpg" alt="TMC Comic Book" width="400">
+//saic sophomore year
+
+}
